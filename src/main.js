ea119f6f71bc0676949c1e976b7a646abcc475f0
thorntonmc <mcthornton5@gmail.com>
2021-06-21 09:08:09 -0400

    stringify package before log

--- src/main.js
+++ src/main.js
@@ -48,6 +48,7 @@ const axios = require('axios');
     try {
 	packagePayload = await axios.post(sncPackageURL, packageBody, defaultHeaders);
     } catch (e) {
+	packagePayload = JSON.stringify(packagePayload);
 	core.setFailed(`failed to create artifact package ${e} \nPayload is ${packageBody}`)
 	return
     }
